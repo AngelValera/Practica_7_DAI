4cb25422cce92bb367a618b7c649b6bff95b38b4
AngelValera <angelvalera.epal@gmail.com>
2016-02-09 18:47:42 +0100

    arreglando directorio media

--- proyecto_DAI/settings.py
+++ proyecto_DAI/settings.py
@@ -116,8 +116,8 @@ STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media/')
 
 
 PASSWORD_HASHERS = (
