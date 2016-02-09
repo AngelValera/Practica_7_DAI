ee36a36412b7779f7649945243b2d5be707341d2
AngelValera <angelvalera.epal@gmail.com>
2016-02-09 18:13:43 +0100

    apaño css

--- proyecto_DAI/urls.py
+++ proyecto_DAI/urls.py
@@ -32,6 +32,7 @@ if not settings.DEBUG:
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        (r'^media/(?P<path>.*)','serve',{'document_root': settings.MEDIA_ROOT}), 
     )
 if settings.DEBUG:
     urlpatterns += patterns(
